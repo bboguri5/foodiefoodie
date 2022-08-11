f6364f22d1c74c1ed17ada5390cbeaf3758533d7
KRDJK <kdf556@naver.com>
2022-08-11 11:51:15 +0900

    [dong-jin] 회원가입, 사업자 권한 요청시 실제 주소 api 적용 완료

--- src/main/java/com/project/foodiefoodie/member/interceptor/AfterLoginInterceptor.java
+++ src/main/java/com/project/foodiefoodie/member/interceptor/AfterLoginInterceptor.java
@@ -1,6 +1,7 @@
 package com.project.foodiefoodie.member.interceptor;
 
 import com.project.foodiefoodie.util.LoginUtils;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Configuration
+@Log4j2
 public class AfterLoginInterceptor implements HandlerInterceptor {
 
     @Override
@@ -17,6 +19,8 @@ public class AfterLoginInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession();
         if (LoginUtils.isLogin(session)){
             String referer = request.getHeader("Referer");
+
+            log.info("exist session ID, redirect!! - {}", referer);
 
             response.sendRedirect(referer);
             return false;

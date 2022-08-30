a81bbd15456714255c85b25334c2f5d4fc85db67
KRDJK <kdf556@naver.com>
2022-08-30 10:18:01 +0900

    [Dong-jin] footer.jsp 수정

--- src/main/java/com/project/foodiefoodie/common/api/KakaoTeamApp.java
+++ src/main/java/com/project/foodiefoodie/common/api/KakaoTeamApp.java
@@ -1,6 +1,6 @@
 package com.project.foodiefoodie.common.api;
 
-public interface KakaoMyApp {
+public interface KakaoTeamApp {
 
     String KAKAO_ADMIN_KEY = "fbb3c962720ce2d6c24e950642acf101";
     String KAKAO_APP_KEY = "76ffb0ca41b0f5b870f948b75368c40c";

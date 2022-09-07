4e6f4c1558ecd93545c032ee558194089cb736ce
jungah <toptoyou@naver.com>
2022-09-07 10:44:05 +0900

    jungah - 시간 포맷하여 표시 완료

--- src/main/java/com/project/foodiefoodie/proBoard/domain/ProBoard.java
+++ src/main/java/com/project/foodiefoodie/proBoard/domain/ProBoard.java
@@ -2,6 +2,7 @@ package com.project.foodiefoodie.proBoard.domain;
 
 import com.project.foodiefoodie.proBoard.dto.FileDTO;
 import com.project.foodiefoodie.proBoard.dto.StoreTimeDTO;
+import com.project.foodiefoodie.util.DateFormatUtils;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,6 +20,8 @@ public class ProBoard extends StoreTimeDTO {
 
     private String businessNo;
     private int promotionBno;
+
+    @Setter(AccessLevel.NONE) // setter 따로 사용하기 위함
     private Date lastUpdated;
     private String content;
     private String title;
@@ -26,4 +29,12 @@ public class ProBoard extends StoreTimeDTO {
     private float avgStarRate;
     private int review_cnt;
     private int report_cnt;
+
+    private String formatDate;
+
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+        this.formatDate = this.lastUpdated != null ? DateFormatUtils.formatTimeVer1(this.lastUpdated) : null;
+    }
 }

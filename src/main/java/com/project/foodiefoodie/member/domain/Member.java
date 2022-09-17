dd1a30c4a4886bf3edd58d94e6567b0733742684
jungah <toptoyou@naver.com>
2022-09-17 19:21:08 +0900

    jungah - 마이페이지 : 수정 오류 해결 -> 정보 수정 가능

--- src/main/java/com/project/foodiefoodie/member/domain/Member.java
+++ src/main/java/com/project/foodiefoodie/member/domain/Member.java
@@ -15,7 +15,8 @@ public class Member {
     private int birth; // 생년월일 8자 19950101
     private String address; // 주소(도로명 주소)
     private String detailAddress; // 상세 주소 : 동, 호수  // ex) 101동 502호
-    private String extraAddress; // 참고 주소 : 행정구역 동, 건물 명   // ex) 가산동, 남성프라자
+    private String extraAddress;
+    private String modifyMember; // 참고 주소 : 행정구역 동, 건물 명   // ex) 가산동, 남성프라자
     private String name; // 이름
     private String gender; // 성별
     private Auth auth; // 권한
